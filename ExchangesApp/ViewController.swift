4ed8318cae8e2da9cf9efcfb970920b642910f97
Wooseong <thehousedone@gmail.com>
2025-07-07 15:08:51 +0900

    chore: SnapKit, Alamofire 추가 / feat: fetchData 함수 작성

--- ExchangesApp/ViewController.swift
+++ ExchangesApp/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  
+  
+  
+  
 
   override func viewDidLoad() {
     super.viewDidLoad()
     print("viewDidLoad")
   }
 
+  private func fetchData<T: Decodable>(url: URL, completion: @escaping (T?) -> Void) {
+    let session = URLSession(configuration: .default)
+    session.dataTask(with: URLRequest(url: url)) {data, response, error in
+      guard let data = data, error == nil else {
+        print("데이터 로드 실패")
+        completion(nil)
+        return
+      }
+      
+      let successRange = 200..<300
+      if let response = response as? HTTPURLResponse,
+         successRange.contains(response.statusCode) {
+        do {
+          let decodedData = try JSONDecoder().decode(T.self, from: data)
+          completion(decodedData)
+        } catch {
+          print("JSON 디코딩 실패: \(error)")
+          completion(nil)
+        }
+      } else {
+        print("응답 오류")
+        completion(nil)
+      }
+    }.resume()
+  }
+  
 
 }
 
