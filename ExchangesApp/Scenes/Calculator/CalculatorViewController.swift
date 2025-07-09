b6d77991090edb93d0ba9ce07819917480c6ae6a
Yoon-Mac <sryoowk@gmail.com>
2025-07-09 15:48:31 +0900

    계산기 화면 구현

--- ExchangesApp/Scenes/Calculator/CalculatorViewController.swift
+++ ExchangesApp/Scenes/Calculator/CalculatorViewController.swift
@@ -1,6 +1,27 @@
-//
-//  CalculatorViewController.swift
-//  ExchangesApp
-//
-//  Created by Yoon on 7/7/25.
-//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class CalculatorViewController: UIViewController {
+    
+    //프로퍼티 선언
+    var currencyItem: CurrencyItem? // 외부에서 주입받기 위한 변수선언
+    
+    private let calculatorView = CalculatorView()
+    
+    // 사용자 정의 뷰로 오버라이드
+    override func loadView() {
+        view = calculatorView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let currencyItem else { return }
+        let viewModel = CalculatorViewModel(currencyItem: currencyItem)
+        calculatorView.setupWithViewModel(viewModel)
+    }
+    
+}
--- ExchangesApp/Scenes/Calculator/CalculatorViewModel.swift
+++ ExchangesApp/Scenes/Calculator/CalculatorViewModel.swift
@@ -1,6 +1,22 @@
-//
-//  CalculatorViewModel.swift
-//  ExchangesApp
-//
-//  Created by Yoon on 7/7/25.
-//
+
+import Foundation
+
+class CalculatorViewModel {
+    
+    var currencyItem: CurrencyItem
+    
+    // init
+    init(currencyItem: CurrencyItem) {
+        self.currencyItem = currencyItem
+    }
+    
+    // 화폐코드 라벨용 텍스트
+    var currencyText: String {
+        return "\(currencyItem.code)"
+    }
+    
+    // 국가명 라벨용 텍스트
+    var counntryNameText: String {
+        return "\(currencyItem.countryName)"
+    }
+}
